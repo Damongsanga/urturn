931bb74f27a32094fa42a69bed8790dc6c347cde
damongsanga <mike0413lata@gmail.com>
2024-04-26 17:59:13 +0900

    Feat: OAuth2 기반 깃허브 소셜로그인 구현

---
+++ back/urturn/src/main/java/com/ssafy/urturn/global/exception/errorcode/ErrorCode.java
@@ -0,0 +1,9 @@
+package com.ssafy.urturn.global.exception.errorcode;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCode {
+    String name();
+    HttpStatus getHttpStatus();
+    String getMessage();
+}
---
+++ back/urturn/src/main/java/com/ssafy/urturn/member/Level.java
@@ -0,0 +1,5 @@
+package com.ssafy.urturn.member;
+
+public enum Level {
+    LEVEL1, LEVEL2, LEVEL3, LEVEL4, LEVEL5
+}
