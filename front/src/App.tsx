eb0fd34fb0917c919edacaeaf8e362d7d912da51
unknown <ygj9605@naver.com>
2024-04-26 15:52:22 +0900

    Feat: MainPage 기본 ui

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,30 +1,16 @@
-import { useState } from 'react';
-
-import { LandingPage } from './pages/landingPage/LandingPage';
-import { WaitingPage } from './pages/waitingPage/WaitingPage';
-import { HeaderBar } from './components/header/HeaderBar';
-
-import './App.css';
 import 'semantic-ui-css/semantic.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import  MainPage  from './pages/mainPage/MainPage.tsx';
 
 function App() {
-	const [isModal, setModal] = useState(false);
-
-	const changeModal = () => {
-		setModal(!isModal);
-		console.log(isModal);
-	};
 
 	return (
 		<>
-			<LandingPage></LandingPage>
-			<HeaderBar $main={true}></HeaderBar>
-			<HeaderBar $ide={true} $mode={1}></HeaderBar>
-			<HeaderBar $ide={true} $mode={2}></HeaderBar>
-			<div>
-				<button onClick={() => changeModal()}> 방 만들기 </button>
-			</div>
-			{isModal && <WaitingPage changeModal={changeModal}></WaitingPage>}
+			<BrowserRouter>
+				<Routes>
+					<Route path='/main' element={<MainPage />} />
+				</Routes>
+			</BrowserRouter>
 		</>
 	);
 }
