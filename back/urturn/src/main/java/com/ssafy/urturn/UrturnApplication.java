8c4458a58c7add7a7e02c37b6d7051cce5ae2e1f
damongsanga <mike0413lata@gmail.com>
2024-04-26 13:49:18 +0900

    Feat: 프로젝트 생성 및 패키지 구조 설정

---
+++ back/urturn/src/main/java/com/ssafy/urturn/UrturnApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.urturn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrturnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrturnApplication.class, args);
+	}
+
+}
---
+++ back/urturn/src/test/java/com/ssafy/urturn/UrturnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.urturn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrturnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
