76c08fa8bb2bfc0374b77447f4d214c92e92a8d5
damongsanga <mike0413lata@gmail.com>
2024-04-29 21:51:11 +0900

    Feat: openvidu url 변경

--- back/src/main/java/com/ssafy/urturn/webrtc/controller/OpenviduController.java
+++ back/src/main/java/com/ssafy/urturn/webrtc/controller/OpenviduController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/openvidu")
+@RequestMapping("/sessions")
 @RestController
 public class OpenviduController {
 
