ac001ca2216eb4479dad7780beaefdec09b4bd3d
damongsanga <mike0413lata@gmail.com>
2024-04-30 10:07:09 +0900

    Feat: history 페이징 slice -> page

--- back/src/main/java/com/ssafy/urturn/history/repository/HistoryCustomRepository.java
+++ back/src/main/java/com/ssafy/urturn/history/repository/HistoryCustomRepository.java
@@ -1,9 +1,10 @@
 package com.ssafy.urturn.history.repository;
 
 import com.ssafy.urturn.history.dto.HistoryResponse;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
 public interface HistoryCustomRepository {
-    Slice<HistoryResponse> getHistories(Long memberId, Pageable pageable);
+    Page<HistoryResponse> getHistories(Long memberId, Pageable pageable);
 }
--- back/src/main/java/com/ssafy/urturn/history/service/HistoryService.java
+++ back/src/main/java/com/ssafy/urturn/history/service/HistoryService.java
@@ -4,6 +4,7 @@ import com.ssafy.urturn.global.util.MemberUtil;
 import com.ssafy.urturn.history.dto.HistoryResponse;
 import com.ssafy.urturn.history.repository.HistoryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
@@ -14,7 +15,7 @@ public class HistoryService {
 
     private final HistoryRepository historyRepository;
 
-    public Slice<HistoryResponse> getHistories(Pageable pageable) {
+    public Page<HistoryResponse> getHistories(Pageable pageable) {
         Long currentMemberId = MemberUtil.getMemberId();
         return historyRepository.getHistories(currentMemberId, pageable);
     }
