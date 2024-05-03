964b00db4ffeb465c8dda82a269343d58ce0ae19
damongsanga <mike0413lata@gmail.com>
2024-05-03 13:46:44 +0900

    Fix: wss -> ws 수정

--- back/src/main/java/com/ssafy/urturn/solving/socket/WebSocketConfig.java
+++ back/src/main/java/com/ssafy/urturn/solving/socket/WebSocketConfig.java
@@ -32,7 +32,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/wss")
+        registry.addEndpoint("/ws")
                 .setAllowedOrigins("*");
 //                .withSockJS();
     }
