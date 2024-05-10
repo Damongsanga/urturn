b754143d80278b6d6a52c592443f2055d4e5b0a7
우창진 <deu03161@naver.com>
2024-05-10 10:31:17 +0900

    fix : test Controller 생성

--- back/src/main/java/com/ssafy/urturn/test/testController.java
+++ back/src/main/java/com/ssafy/urturn/test/testController.java
@@ -1,7 +1,18 @@
 package com.ssafy.urturn.test;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import com.ssafy.urturn.solving.service.RoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RequestMapping("/test")
+@RestController
+@RequiredArgsConstructor
 public class testController {
-
+    private final RoomService roomService;
+    @GetMapping("/test")
+    public ResponseEntity<?> checkRoomEntry(){
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("성공");
+    }
 }
