fed8046013d9382f51572a97e827a02d86359616
이혜지 <lee02g29@naver.com>
2024-05-16 14:43:50 +0900

    205 : 빌드 오류 수정

--- front/src/pages/trainsPage/TransPage.tsx
+++ front/src/pages/trainsPage/TransPage.tsx
@@ -34,7 +34,6 @@ export const TransPage = () => {
 	const { next } = useParams();
 	const sec = useRef(0);
 	const navigate = useNavigate();
-	const roomStore = useRoomStore();
 
 	const START_TIME = 3;
 
