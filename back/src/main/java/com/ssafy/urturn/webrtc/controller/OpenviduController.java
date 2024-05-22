afdbf114c0c0f82142e421cf649572662cc37833
damongsanga <mike0413lata@gmail.com>
2024-05-22 22:52:48 +0900

    Refactor: simple refactoring

--- back/src/main/java/com/ssafy/urturn/webrtc/controller/OpenviduController.java
+++ back/src/main/java/com/ssafy/urturn/webrtc/controller/OpenviduController.java
@@ -1,24 +1,14 @@
 package com.ssafy.urturn.webrtc.controller;
 
-import io.openvidu.java.client.Connection;
-import io.openvidu.java.client.ConnectionProperties;
-import io.openvidu.java.client.OpenVidu;
-import io.openvidu.java.client.OpenViduHttpException;
-import io.openvidu.java.client.OpenViduJavaClientException;
-import io.openvidu.java.client.Session;
-import io.openvidu.java.client.SessionProperties;
+import io.openvidu.java.client.*;
 import jakarta.annotation.PostConstruct;
-import java.util.HashMap;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RequestMapping("/sessions")
 @RestController
@@ -42,9 +32,6 @@ public class OpenviduController {
         throws OpenViduJavaClientException, OpenViduHttpException {
         SessionProperties properties = SessionProperties.fromJson(params).build();
         Session session = openvidu.createSession(properties);
-
-        // 세션 만들면서 동시에 해야하는 로직
-
         return new ResponseEntity<>(session.getSessionId(), HttpStatus.OK);
     }
 
