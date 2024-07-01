e62f485d3d3c01e0ec1b41f6fdfcb3af7b3526b9
damongsanga <mike0413lata@gmail.com>
2024-07-01 21:07:23 +0900

    Refactor: grading server 용 프로젝트 추가

---
+++ grading/src/main/java/com/ssafy/urturn/grading/GradingApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.urturn.grading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradingApplication.class, args);
+	}
+
+}
