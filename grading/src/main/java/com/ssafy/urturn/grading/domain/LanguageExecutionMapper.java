58ab6ff2943080a830e13642ec8ea36737435794
damongsanga <mike0413lata@gmail.com>
2024-07-06 00:05:36 +0900

    feat: languageId - 전략 bean 매핑

---
+++ grading/src/main/java/com/ssafy/urturn/grading/domain/LanguageExecutionMapper.java
@@ -0,0 +1,19 @@
+package com.ssafy.urturn.grading.domain;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LanguageExecutionMapper {
+
+    public static String getExecutionService(int languageId) {
+        if (languageId == 1) {
+            return "javaExecutionStrategy";
+        } else if (languageId == 2) {
+            return "pythonExecutionStrategy";
+        } else if (languageId == 3) {
+            return "javascriptExecutionStrategy";
+        } else {
+            throw new IllegalArgumentException("Unsupported language ID: " + languageId);
+        }
+    }
+}
