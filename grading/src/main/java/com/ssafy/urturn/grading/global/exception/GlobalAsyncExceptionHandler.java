283b94c3b1606e9c513819683d95ed935004b0e7
damongsanga <mike0413lata@gmail.com>
2024-07-06 00:02:46 +0900

    feat: 비동기 로직 추가

---
+++ grading/src/main/java/com/ssafy/urturn/grading/global/exception/GlobalAsyncExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.ssafy.urturn.grading.global.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+@Component
+public class GlobalAsyncExceptionHandler implements AsyncUncaughtExceptionHandler {
+
+    @Override
+    public void handleUncaughtException(Throwable ex, Method method, Object... params) {
+        log.error("Method name : {}, param Count : {}\n\nException Cause -{}", method.getName(), params.length, ex.getMessage());
+    }
+
+}
