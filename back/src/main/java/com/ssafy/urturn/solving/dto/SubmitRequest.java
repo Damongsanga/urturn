51bef4d32d01a11c484501152b2941b3f642fc61
damongsanga <mike0413lata@gmail.com>
2024-07-14 22:57:51 +0900

    test: 테스트를 위한 builder, 생성자 추가

--- back/src/main/java/com/ssafy/urturn/solving/dto/SubmitRequest.java
+++ back/src/main/java/com/ssafy/urturn/solving/dto/SubmitRequest.java
@@ -2,15 +2,20 @@ package com.ssafy.urturn.solving.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ssafy.urturn.problem.Language;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SubmitRequest {
     private static final Logger log = LoggerFactory.getLogger(SubmitRequest.class);
     private String code;
-
     private String roomId;
     private Language language;
     @JsonProperty("algoQuestionId")
